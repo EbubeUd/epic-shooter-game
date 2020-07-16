92efc708774a550dfae8bb9c1240658b65e308ec
Precious Kosisochukwu <kcokolo10@gmail.com>
2020-07-16 22:48:20 +0100

    Inital Commit

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rigidBody;
+    private Vector2 moveAmount;
+    private Animator aminator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        aminator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * Speed;
+
+        //Changing the MotionState on Key held down
+        if (moveInput != Vector2.zero)
+        {
+            aminator.SetInteger("MotionState", (int)PlayMotionState.Run);
+        }
+        else
+        {
+            aminator.SetInteger("MotionState", (int)PlayMotionState.Idle);
+        }
+
+    }
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + moveAmount * Time.fixedDeltaTime);
+    }
+}
